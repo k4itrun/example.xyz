abb3eda8600094ecfe14dc319fc0c0297584a3fc
k4itrun <103044629+k4itrun@users.noreply.github.com>
2025-04-13 15:47:44 -0500

    my work

--- src/app.js
+++ src/app.js
@@ -1,42 +1,33 @@
 import express from 'express';
 import path from 'path';
 import minifyHTML from 'express-minify-html-terser';
-import {
-  navLinks,
-  site,
-  openGraph,
-  twitter,
-  fonts,
-  adsense,
-  redirects,
-  port
-} from './config.js';
+import { navLinks, site, openGraph, twitter, fonts, adsense, redirects, port } from './config.js';
 
 const app = express();
 const PORT = process.env.PORT || port;
 
 // Middleware =>
-app.use(minifyHTML({
-  override: true,
-  exception_url: false,
-  htmlMinifier: {
-    removeComments: true,
-    collapseWhitespace: true,
-    collapseBooleanAttributes: true,
-    removeAttributeQuotes: true,
-    removeEmptyAttributes: true,
-    minifyJS: true
-  }
-}));
+app.use(
+  minifyHTML({
+    override: true,
+    exception_url: false,
+    htmlMinifier: {
+      removeComments: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeAttributeQuotes: true,
+      removeEmptyAttributes: true,
+      minifyJS: true,
+    },
+  })
+);
 
-app.use(express.static('public'));
-app.use(express.json());
+app.use(express.static(path.resolve('src', 'public', 'assets')));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.set('view engine', 'ejs');
-app.set('views', path.resolve('views'));
-
-app.use(express.static(path.resolve('public', 'assets')));
+app.set('views', path.resolve('src', 'views'));
 
 // Main routes =>
 app.get('/', (req, res) => {
---
+++ src/config.js
@@ -0,0 +1,71 @@
+// Nav links =>
+export const navLinks = [
+  {
+    name: 'Home',
+    url: '/',
+    external: false,
+  },
+  {
+    name: 'Discord',
+    url: '/discord',
+    external: true,
+  },
+  {
+    name: 'YouTube',
+    url: '/youtube',
+    external: false,
+  },
+];
+
+// Site config =>
+export const site = {
+  name: 'example.xyz',
+  title: 'example.xyz',
+  description: 'A simple Express.js application using EJS for templating.',
+  keywords: ['expressjs', 'nodejs', 'commonjs', 'ejs'].join(', '),
+  url: 'https://example.xyz/',
+  favicon: '/img/logo/default.png',
+  themeColor: '#ddac3d',
+  image: 'https://expressjs.com/images/og.png',
+  twitterHandle: '@express',
+};
+
+export const openGraph = {
+  type: 'article',
+  siteName: 'example.xyz',
+  description: 'A simple Express.js application using EJS for templating.',
+  image: 'https://expressjs.com/images/og.png',
+  imageAlt: 'expressjs',
+};
+
+export const twitter = {
+  creator: '@express',
+  site: '@express',
+  cardType: 'summary_large_image',
+  imageAlt: '',
+};
+
+export const fonts = {
+  fonts: ['https://fonts.googleapis.com/css?family=Poppins:300,400,600,700,800,900&display=swap', 'https://fonts.googleapis.com/css?family=Montserrat:400,600,700,800,900&display=swap', 'https://fonts.googleapis.com/css?family=Lato:300,400,600,700,800,900&display=swap'],
+};
+
+export const adsense = {
+  client: 'ca-pub-8275781379114487',
+};
+
+// Redirects =>
+export const redirects = [
+  {
+    source: '/discord',
+    destination: 'https://discord.gg',
+    permanent: true,
+  },
+  {
+    source: '/youtube',
+    destination: 'https://youtube.com',
+    permanent: true,
+  },
+];
+
+// Default Port =>
+export const port = 3001;
